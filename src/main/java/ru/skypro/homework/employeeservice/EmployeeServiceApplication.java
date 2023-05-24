25af0ebda1a23015ac3bb63cb5074d5a8c70e8e2
ya121 <ya7771@yandex.ru>
2023-05-24 11:03:10 +0300

    Начальный коммит к ДЗ № 3_9 Spring Boot

---
+++ src/main/java/ru/skypro/homework/employeeservice/EmployeeServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.skypro.homework.employeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+    public static void main(String[] args) {SpringApplication.run(EmployeeServiceApplication.class, args);}
+
+}
